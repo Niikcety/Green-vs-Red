ad99ff464cb716eaf7d3a96ea6cd91e3492868fa
Niikcety <nikolaineshev1999@gmail.com>
2020-07-22 23:02:51 +0300

    added next generation generator method

--- grid/grid.py
+++ grid/grid.py
@@ -1,7 +1,142 @@
 class Grid:
     def __init__(self):
-        self. grid = []
+        self.x = -1
+        self.y = -1
+        self.grid = []
 
     def add_row(self, row):
         row_list = list(row)
         self.grid.append(row_list)
+
+    def check_for_green_neighbours_corner_cells(self, curr_x, curr_y):
+        # check on (0, 0), (0, y - 1), (x -1 , 0), (x - 1, y - 1)
+        # needs testing
+        green_number = 0
+        if curr_x == 0 and curr_y == 0:
+            if self.grid[curr_x][curr_y + 1] == '1':
+                green_number += 1
+            if self.grid[curr_x + 1][curr_y] == '1':
+                green_number += 1
+            if self.grid[curr_x + 1][curr_y + 1] == '1':
+                green_number += 1
+        elif curr_x == 0 and curr_y == self.y - 1:
+            if self.grid[curr_x][curr_y - 1] == '1':
+                green_number += 1
+            if self.grid[curr_x + 1][curr_y] == '1':
+                green_number += 1
+            if self.grid[curr_x + 1][curr_y - 1] == '1':
+                green_number += 1
+        elif curr_x == self.x - 1 and curr_y == 0:
+            if self.grid[curr_x][curr_y + 1] == '1':
+                green_number += 1
+            if self.grid[curr_x - 1][curr_y] == '1':
+                green_number += 1
+            if self.grid[curr_x - 1][curr_y + 1] == '1':
+                green_number += 1
+        else:
+            if self.grid[curr_x][curr_y - 1] == '1':
+                green_number += 1
+            if self.grid[curr_x - 1][curr_y] == '1':
+                green_number += 1
+            if self.grid[curr_x - 1][curr_y - 1] == '1':
+                green_number += 1
+        return green_number
+
+    def check_for_green_neighbours_bound_line_cells(self, curr_x, curr_y):
+        # check on (0, y), (x, 0), (x - 1, y), (x, y - 1)
+        green_number = 0
+        if curr_x == 0:
+            if self.grid[curr_x][curr_y + 1] == '1':
+                green_number += 1
+            if self.grid[curr_x][curr_y - 1] == '1':
+                green_number += 1
+            if self.grid[curr_x + 1][curr_y - 1] == '1':
+                green_number += 1
+            if self.grid[curr_x + 1][curr_y] == '1':
+                green_number += 1
+            if self.grid[curr_x + 1][curr_y + 1] == '1':
+                green_number += 1
+        elif curr_x == self.x - 1:
+            if self.grid[curr_x][curr_y + 1] == '1':
+                green_number += 1
+            if self.grid[curr_x][curr_y - 1] == '1':
+                green_number += 1
+            if self.grid[curr_x - 1][curr_y - 1] == '1':
+                green_number += 1
+            if self.grid[curr_x - 1][curr_y] == '1':
+                green_number += 1
+            if self.grid[curr_x - 1][curr_y + 1] == '1':
+                green_number += 1
+        elif curr_y == 0:
+            if self.grid[curr_x][curr_y + 1] == '1':
+                green_number += 1
+            if self.grid[curr_x - 1][curr_y] == '1':
+                green_number += 1
+            if self.grid[curr_x - 1][curr_y + 1] == '1':
+                green_number += 1
+            if self.grid[curr_x + 1][curr_y] == '1':
+                green_number += 1
+            if self.grid[curr_x + 1][curr_y + 1] == '1':
+                green_number += 1
+        else:
+            if self.grid[curr_x][curr_y - 1] == '1':
+                green_number += 1
+            if self.grid[curr_x - 1][curr_y - 1] == '1':
+                green_number += 1
+            if self.grid[curr_x - 1][curr_y] == '1':
+                green_number += 1
+            if self.grid[curr_x + 1][curr_y] == '1':
+                green_number += 1
+            if self.grid[curr_x + 1][curr_y - 1] == '1':
+                green_number += 1
+        return green_number
+
+    def check_for_green_neighbours_inner_cells(self, curr_x, curr_y):
+        green_number = 0
+        if self.grid[curr_x][curr_y - 1] == '1':
+            green_number += 1
+        if self.grid[curr_x][curr_y + 1] == '1':
+            green_number += 1
+        if self.grid[curr_x - 1][curr_y - 1] == '1':
+            green_number += 1
+        if self.grid[curr_x - 1][curr_y + 1] == '1':
+            green_number += 1
+        if self.grid[curr_x - 1][curr_y] == '1':
+            green_number += 1
+        if self.grid[curr_x + 1][curr_y - 1] == '1':
+            green_number += 1
+        if self.grid[curr_x + 1][curr_y] == '1':
+            green_number += 1
+        if self.grid[curr_x + 1][curr_y + 1] == '1':
+            green_number += 1
+        return green_number
+
+    def create_next_generation(self):
+        next_generation = []
+        for r in range(0, self.x):
+            next_gen_row = []
+            for c in range(0, self.y):
+                if self.grid[r][c] == '0':
+                    if (r == 0 and (c == self.y - 1 or c == 0)) or (r == self.x - 1 and (c == self.y - 1 or c == 0)):
+                        green_neighbours = self.check_for_green_neighbours_corner_cells(r, c)
+                    elif c == 0 or c == self.y - 1 or r == 0 or r == self.x - 1:
+                        green_neighbours = self.check_for_green_neighbours_bound_line_cells(r, c)
+                    else:
+                        green_neighbours = self.check_for_green_neighbours_inner_cells(r, c)
+                    if green_neighbours == 3 or green_neighbours == 6:
+                        next_gen_row.append('1')
+                    else:
+                        next_gen_row.append('0')
+                else:
+                    if (r == 0 and (c == self.y - 1 or c == 0)) or (r == self.x - 1 and (c == self.y - 1 or c == 0)):
+                        green_neighbours = self.check_for_green_neighbours_corner_cells(r, c)
+                    elif c == 0 or c == self.y - 1 or r == 0 or r == self.x - 1:
+                        green_neighbours = self.check_for_green_neighbours_bound_line_cells(r, c)
+                    else:
+                        green_neighbours = self.check_for_green_neighbours_inner_cells(r, c)
+                    if green_neighbours == 2 or green_neighbours == 3 or green_neighbours == 6:
+                        next_gen_row.append('1')
+                    else:
+                        next_gen_row.append('0')
+            next_generation.append(next_gen_row)
+        self.grid = next_generation
--- utils/utils.py
+++ utils/utils.py
@@ -4,15 +4,13 @@ def check_if_size_is_correct(size):
     y = int(size_list[1])
     if x < 0 or y < 0 or x > y:
         return False
-    else:
-        return True
+    return True
 
 
 def check_if_row_size_is_correct(row, size):
     if len(row) == size:
         return True
-    else:
-        return False
+    return False
 
 
 def check_if_row_contains_only_valid_chars(row):
@@ -24,10 +22,18 @@ def check_if_row_contains_only_valid_chars(row):
 
 def check_if_wanted_values_are_valid(wanted):
     wanted_list = wanted.split(', ')
-    wanted_x = wanted_list[0]
-    wanted_y = wanted_list[1]
-    wanted_turns = wanted_list[2]
+    wanted_x = int(wanted_list[0])
+    wanted_y = int(wanted_list[1])
+    wanted_turns = int(wanted_list[2])
     if wanted_x < 0 or wanted_y < 0 or wanted_turns < 0:
         return False
-    else:
-        return True
+    return True
+
+
+def check_if_wanted_values_are_in_the_bounds(wanted, x, y):
+    wanted_list = wanted.split(', ')
+    wanted_x = int(wanted_list[0])
+    wanted_y = int(wanted_list[1])
+    if wanted_x >= x or wanted_y >= y:
+        return False
+    return True

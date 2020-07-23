771a2cff8e0429b066ded7be89c4901210233fe4
Niikcety <nikolaineshev1999@gmail.com>
2020-07-23 13:03:02 +0300

    added tests for userinput

---
+++ userinput/test_userinput.py
@@ -0,0 +1,69 @@
+import unittest
+from userinput import UserInput
+
+
+class TestUserInput(unittest.TestCase):
+    def test_set_size_with_invalid_size_should_raise_value_error(self):
+        user_input = UserInput()
+        exc = None
+        size = '3,3'
+
+        try:
+            user_input.set_size(size)
+        except ValueError as err:
+            exc = err
+
+        self.assertIsNotNone(exc)
+        self.assertEquals(str(exc), "Invalid input! Size must be like 'x, y'.")
+
+    def test_set_size_with_valid_size_should_set_grid_properties(self):
+        user_input = UserInput()
+        size = '3, 4'
+
+        user_input.set_size(size)
+
+        self.assertEquals(user_input.grid.x, 3)
+        self.assertEquals(user_input.grid.y, 4)
+
+    def test_set_wanted_with_invalid_wanted_should_raise_value_error(self):
+        user_input = UserInput()
+        exc = None
+        wanted = '3,3,50'
+
+        try:
+            user_input.set_wanted(wanted)
+        except ValueError as err:
+            exc = err
+
+        self.assertIsNotNone(exc)
+        self.assertEquals(str(exc), "Invalid expected input it should be like: 'x, y, turns'.")
+
+    def test_set_wanted_with_out_of_bounds_should_raise_value_error(self):
+        user_input = UserInput()
+        user_input.grid.x = 2
+        user_input.grid.y = 2
+        wanted = '2, 2, 50'
+        exc = None
+
+        try:
+            user_input.set_wanted(wanted)
+        except ValueError as err:
+            exc = err
+
+        self.assertIsNotNone(exc)
+        self.assertEquals(str(exc), "Invalid expected input it should be like: 'x, y, turns'.")
+
+    def test_set_wanted_with_valid_input_should_set_userinput_properties(self):
+        user_input = UserInput()
+        user_input.set_size('4, 4')
+        wanted = '2, 3, 30'
+
+        user_input.set_wanted(wanted)
+
+        self.assertEquals(user_input.wanted_x, 2)
+        self.assertEquals(user_input.wanted_y, 3)
+        self.assertEquals(user_input.turns, 30)
+
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ userinput/userinput.py
@@ -0,0 +1,45 @@
+from utils.utils import check_if_size_is_correct, check_if_row_size_is_correct, \
+    check_if_row_contains_only_valid_chars, check_if_wanted_values_are_valid, check_if_wanted_values_are_in_the_bounds
+from grid.grid import Grid
+
+
+class UserInput:
+    def __init__(self):
+        self.wanted_x = -1
+        self.wanted_y = -1
+        self.turns = -1
+        self.grid = Grid()
+
+    def set_size(self, size):
+        if check_if_size_is_correct(size):
+            size_list = size.split(', ')
+            self.grid.x = int(size_list[0])
+            self.grid.y = int(size_list[1])
+        else:
+            raise ValueError("Invalid input! Size must be like 'x, y'.")
+
+    def set_grid(self):
+        for row in range(0, self.grid.x):
+            new_row = input('Enter row #{}: '.format(row))
+            if check_if_row_size_is_correct(new_row, self.grid.y) and check_if_row_contains_only_valid_chars(new_row):
+                self.grid.add_row(new_row)
+            else:
+                raise ValueError(
+                    "Invalid row it should only contain '1' and '0' and must be with length {}".format(self.grid.y))
+
+    def set_wanted(self, wanted):
+        if check_if_wanted_values_are_valid(wanted) and \
+                check_if_wanted_values_are_in_the_bounds(wanted, self.grid.x, self.grid.y):
+            wanted_list = wanted.split(', ')
+            self.wanted_x = int(wanted_list[0])
+            self.wanted_y = int(wanted_list[1])
+            self.turns = int(wanted_list[2])
+        else:
+            raise ValueError("Invalid expected input it should be like: 'x, y, turns'.")
+
+    def get_input(self):
+        size = input("Enter the size of the grid in format: 'x, y'. ")
+        self.set_size(size)
+        self.set_grid()
+        wanted = input("Enter the coordinates of the cell and the round in format: 'x, y, round'. ")
+        self.set_wanted(wanted)
